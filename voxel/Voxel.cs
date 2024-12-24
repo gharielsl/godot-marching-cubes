79b84093750451ec71f2b8a634c5b1877acc3f88
gharielsl <slavinskyariel@gmail.com>
2024-12-24 13:01:03 +0200

    first commit

---
+++ voxel/Voxel.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class Voxel
+{
+    public static Dictionary<int, Voxel> Voxels = new Dictionary<int, Voxel>();
+    public static void RegisterVoxel(Voxel voxel)
+    {
+
+    }
+}
